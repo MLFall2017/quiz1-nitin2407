caf6865dad1012acd8ddf154372b2fa186d89b38
Nitin <nitinpurohit258@gmail.com>
2017-09-22 10:30:08 -0400

    first commit

---
+++ quiz1.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def pca(input):
+    
+    #get input and skip headers
+    input = np.genfromtxt(input,delimiter=",",skip_header=1) 
+
+    #find correlation coeficient matrix
+    cov_in = np.corrcoef(input.T)
+
+    #find eigen values and vectors
+    eig_val, eig_vec = np.linalg.eig(cov_in)
+
+    #pairing of eigen values and vectors
+    eig_pairs = [(np.abs(eig_val[i]),eig_vec[:,i]) for i in range(len(eig_val))]
+
+    #sorting and finding cumulative matrix for eigen values
+    eig_pairs.sort()
+    eig_pairs.reverse()
+    tot = sum(eig_val)
+    eigval_per = [(i/tot)*100 for i in sorted(eig_val,reverse=True)]
+    cum_sum = np.cumsum(eigval_per)
+
+    #finding the new reduced data matrix
+    matrix_w = np.hstack((eig_pairs[0][1].reshape(3,1),eig_pairs[1][1].reshape(3,1)))
+    y = input.dot(matrix_w)
+    return y
+
+#getting the dataset
+data = np.genfromtxt("dataset_1.csv",delimiter=",",skip_header=1)
+
+#seperating indivisual columns
+x = data.T[0]
+y = data.T[1]
+z = data.T[2]
+
+#finding variance for each variable
+var_x = np.var(x)
+var_y = np.var(y)
+var_z = np.var(z)
+
+#finding covariance
+cov_xy = np.cov(x,y)
+cov_yz = np.cov(y,z)
+
+#getting pca output
+out = pca("dataset_1.csv")
+
